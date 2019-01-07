e0e5dda2af469d3481fb9104d907c460d53cb799
zyao89 <zyao89@gmail.com>
2019-01-07 18:45:53 +0800

    :sparkles: update

--- libs/createApi.js
+++ libs/createApi.js
@@ -60,8 +60,8 @@ module.exports = function(fileNames, root) {
 
         const result = {
             name,
-            prefix: '<svg version="1.1" xmlns="http://www.w3.org/2000/svg"><g><rect opacity="0"></rect>',
-            suffix: '</g></svg>',
+            xmlns: 'http://www.w3.org/2000/svg',
+            version: '1.1',
             icons,
         };
 
